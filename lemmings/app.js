1b52c6bb4e8ce5abd1b3011bbc9373abac84e684
spritus <spritus@gmail.com>
2023-05-22 23:12:42 +0300

    e

--- lemmings/app.js
+++ lemmings/app.js
@@ -1,11 +1,14 @@
 const lemmings = [];
 const walls = [];
+const generators = [];
+const doors = [];
+
 const screen = document.getElementById("screen");
 
 const W = 900;
 const H = 600;
 const LS = 20;
-const speed = 2;
+const speed = 0.5;
 
 class Wall {
     constructor(x, y, w, h) {
@@ -25,10 +28,10 @@ class Wall {
 }
 
 class Lemming {
-    constructor(x, y) {
+    constructor(x, y, rot) {
         this.x = x;
         this.y = y;
-        this.rot = 1;
+        this.rot = rot;
 
         this.body = document.createElement("div");
         this.body.classList.add("lemming");
@@ -44,40 +47,54 @@ class Lemming {
                 this.y < wall.y + wall.h &&
                 this.y + LS > wall.y) {
                 this.rot = 1 + this.rot % 4;
+                this.x = this.ox;
+                this.y = this.oy;
             }
         }
 
+        for (let door of doors) {
+            if (this.x < door.x + 30 &&
+                this.x + LS > door.x &&
+                this.y < door.y + 30 &&
+                this.y + LS > door.y) {
+                this.body.remove();
+            }
+        }
+
+        this.ox = this.x;
+        this.oy = this.y;
+
         switch (this.rot) {
             case 1:
-                if (this.y <= 0) this.rot = 1 + this.rot % 4;
-                else {
-                    this.oy = this.y;
-                    this.y -= speed;
+                if (this.y < 0) {
+                    this.rot = 1 + this.rot % 4;
+                    this.y = 0;
                 }
+                else this.y -= speed;
                 break;
 
             case 2:
-                if (this.x >= W - LS) this.rot = 1 + this.rot % 4;
-                else {
-                    this.ox = this.x;
-                    this.x += speed;
+                if (this.x > W - LS) {
+                    this.rot = 1 + this.rot % 4;
+                    this.x = W - LS;
                 }
+                else this.x += speed;
                 break;
 
             case 3:
-                if (this.y >= H - LS) this.rot = 1 + this.rot % 4;
-                else {
-                    this.oy = this.y;
-                    this.y += speed;
+                if (this.y > H - LS) {
+                    this.rot = 1 + this.rot % 4;
+                    this.y = H - LS;
                 }
+                else this.y += speed;
                 break;
 
             case 4:
-                if (this.x <= 0) this.rot = 1 + this.rot % 4;
-                else {
-                    this.ox = this.x;
-                    this.x -= speed;
+                if (this.x < 0) {
+                    this.rot = 1 + this.rot % 4;
+                    this.x = 0;
                 }
+                else this.x -= speed;
                 break;
         }
 
@@ -86,19 +103,66 @@ class Lemming {
     }
 }
 
+class Generator {
+    constructor(x, y, lc, rot) {
+        this.x = x;
+        this.y = y;
+        this.lc = lc;
+        this.rot = rot;
+        this.body = document.createElement("div");
+        this.body.classList.add("generator");
+        this.body.style.left = this.x + "px";
+        this.body.style.top = this.y + "px";
+        screen.append(this.body);
+    }
+
+    action() {
+        if (this.lc > 0) {
+            setTimeout(() => {
+                lemmings.push(new Lemming(this.x + 8, this.y + 8, this.rot));
+            }, 1000 * this.lc);
+            this.lc--;
+        }
+    }
+}
+
+class Doors {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.body = document.createElement("div");
+        this.body.classList.add("door");
+        this.body.style.left = this.x + "px";
+        this.body.style.top = this.y + "px";
+        screen.append(this.body);
+    }
+}
+
 walls.push(new Wall(100, 150, 400, 50));
 walls.push(new Wall(100, 350, 400, 50));
+walls.push(new Wall(500, 150, 50, 150));
+walls.push(new Wall(50, 150, 50, 200));
+
+generators.push(new Generator(150, 250, 5, 1));
+generators.push(new Generator(270, 450, 15, 3));
+generators.push(new Generator(580, 400, 15, 4));
+
+doors.push(new Doors(270, 550));
 
 function lifeLoop() {
     for (lem of lemmings) {
         lem.action();
     }
 
+    for (gen of generators) {
+        gen.action();
+    }
+
     requestAnimationFrame(lifeLoop);
 }
 
 screen.onclick = (e) => {
-    lemmings.push(new Lemming(e.offsetX, e.offsetY));
+    //lemmings.push(new Lemming(e.offsetX, e.offsetY));
 }
 
 lifeLoop();
