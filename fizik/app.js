8f463a232fed9687c2446e5e3051a3abdcab941e
spritus <spritus@gmail.com>
2023-09-12 21:19:09 +0300

    e

--- fizik/app.js
+++ fizik/app.js
@@ -1,4 +1,4 @@
-var Engine = Matter.Engine,
+const Engine = Matter.Engine,
     Render = Matter.Render,
     World = Matter.World,
     Bodies = Matter.Bodies,
@@ -13,13 +13,13 @@ const HEIGHT = 550;
 
 const CX = 120, CY = 350;
 
-// create an engine
-var engine = Engine.create();
+// Motoru oluştur
+const engine = Engine.create();
 const world = engine.world;
 const game = document.getElementById("game");
 
-// create a renderer
-var render = Render.create({
+// Rendereri oluştur
+const render = Render.create({
     element: game,
     engine: engine,
     options: {
@@ -29,13 +29,7 @@ var render = Render.create({
     }
 });
 
-// create two boxes and a ground
-var box = Bodies.rectangle(500, -200, 80, 80, {
-    render: {
-        color: "red"
-    }
-});
-
+// Zemin için texture yap
 const image = Bodies.rectangle(400, 275, 1, 1, {
     isStatic: true,
     render: {
@@ -46,36 +40,46 @@ const image = Bodies.rectangle(400, 275, 1, 1, {
         }
     }
 });
-
 image.collisionFilter = {};
 
+// Zemini oluştur
 const level1 = "112,547 800,549 800,241 785,243 765,233 746,238 728,257 726,292 694,351 680,375 626,376 610,363 587,355 567,349 563,327 543,306 545,238 523,214 497,212 467,214 441,242 441,299 441,330 439,351 397,385 392,325 374,303 340,309 330,340 330,472 317,485 300,499 234,522 179,525 146,533";
-
-var ground = Bodies.fromVertices(545, 375, Matter.Vertices.fromPath(level1),
+const ground = Bodies.fromVertices(545, 375, Matter.Vertices.fromPath(level1),
     {
         isStatic: true,
+        label: "ground",
         render: {
             visible: false
         }
     }, true);
 
-var pyramid = Composites.pyramid(100, 200, 9, 10, 0, 0, function (x, y) {
-    return Bodies.rectangle(x, y, 25, 40);
+// Bomba özellikleri
+const bombOptions = {
+    density: 0.004,
+    friction: 0,
+    label: "bombe",
+    render: {
+        fillStyle: "red"
+    }
+};
+
+// İlk bombayı oluştur
+let bomb = Bodies.circle(CX, CY, 7, bombOptions);
+
+// Mancınık oluştur
+const elastic = Constraint.create({
+    pointA: { x: CX, y: CY },
+    bodyB: bomb,
+    stiffness: 0.01
 });
 
-var rockOptions = { density: 0.004 },
-    rock = Bodies.circle(CX, CY, 7, rockOptions),
-    elastic = Constraint.create({
-        pointA: { x: CX, y: CY },
-        bodyB: rock,
-        stiffness: 0.05
-    });
-
-var mouse = Mouse.create(render.canvas),
+// Mancınık bomba ve mouse bağlantılarını oluştur
+const mouse = Mouse.create(render.canvas),
     mouseConstraint = MouseConstraint.create(engine, {
         mouse: mouse,
         constraint: {
-            stiffness: 0.2,
+            stiffness: 0.5,
+            label: "elastic",
             render: {
                 visible: false
             }
@@ -84,23 +88,36 @@ var mouse = Mouse.create(render.canvas),
 
 World.add(world, mouseConstraint);
 
-// keep the mouse in sync with rendering
+// Mouse işlemleri
 render.mouse = mouse;
-
-Events.on(engine, 'afterUpdate', function () {
-    if (mouseConstraint.mouse.button === -1 && (rock.position.x > CX + 20 || rock.position.y < CY - 20)) {
-        rock = Bodies.circle(CX, CY, 7, rockOptions);
-        World.add(engine.world, rock);
-        elastic.bodyB = rock;
+Events.on(engine, "afterUpdate", function () {
+    if (mouseConstraint.mouse.button === -1 && (bomb.position.x > CX + 20 || bomb.position.y < CY - 20)) {
+        bomb = Bodies.circle(CX, CY, 7, bombOptions);
+        elastic.bodyB = bomb;
     }
 });
 
-// add all of the bodies to the world
-World.add(engine.world, [ground, image, rock, elastic]);
+// Bombanın ilk temas ettiği yerde yapılacaklar
+let touch = false;
+Events.on(engine, "collisionStart", function (event) {
+    let obj = event.pairs[0].bodyB;
 
-// run the renderer
+    if (obj.label == "bombe" && !touch) {
+        obj.render.fillStyle = "green";
+        touch = true;
+        setTimeout(() => {
+            Matter.Composite.remove(engine.world, obj);
+            World.add(engine.world, bomb);
+            touch = false;
+        }, 3000);
+    }
+});
+
+// Tüm materyalleri dünyaya ekle
+World.add(engine.world, [ground, image, bomb, elastic]);
+
+// Renderi çalıştır
 Render.run(render);
 
-// run the engine
+// Motoru çalıştır
 Engine.run(engine);
-
