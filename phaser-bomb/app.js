fbfc0adaa66061d3f4275bad75711b89cc6be950
spritus <spritus@gmail.com>
2023-10-14 02:42:23 +0300

    e

--- phaser-bomb/app.js
+++ phaser-bomb/app.js
@@ -8,7 +8,13 @@ class Bombs extends Phaser.Scene {
 
     create() {
         this.add.image(150, 0, "level1").setOrigin(0, 0);
+        this.matter.world.setBounds(0, 0, W, H, true, true, false, true);
+        const path = "112,547 800,549 800,241 785,243 765,233 746,238 728,257 726,292 694,351 680,375 626,376 610,363 587,355 567,349 563,327 543,306 545,238 523,214 497,212 467,214 441,242 441,299 441,330 439,351 397,385 392,325 374,303 340,309 330,340 330,472 317,485 300,499 234,522 179,525 146,533";
+        const verts = this.matter.verts.fromPath(path);
+        this.matter.add.fromVertices(696, 375, verts, { isStatic: true }, true, 0.01, 10);
 
+        const bomb = this.matter.add.image(500, 0, "bomb", 5);
+        bomb.setCircle();
     }
 }
 
@@ -21,7 +27,7 @@ const game = new Phaser.Game({
     physics: {
         default: "matter",
         matter: {
-            debug: false,
+            debug: true,
             enableSleeping: true
         }
     },
